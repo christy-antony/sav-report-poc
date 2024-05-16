b93491df8b4cb21de2007923ca3710a5907c54b8
Christy Antony <christy.antony@parallel-minds.com>
2024-05-16 18:04:22 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import html2canvas from 'html2canvas';
+import * as jspdf from 'jspdf';
+
 
 function App() {
+  const [htmlContent, setHtmlContent] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchHtmlContent = async () => {
+      try {
+        const response = await fetch('http://localhost:5149/api/Templates/template1.html');
+        if (!response.ok) {
+          throw new Error('Failed to fetch HTML content');
+        }
+        const htmlData = await response.text();
+        setHtmlContent(htmlData);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchHtmlContent();
+  }, []); 
+
+  const exportToPdf = () => {
+    const element = document.getElementById('contentToExport');
+
+    html2canvas(element)
+      .then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jspdf.jsPDF();
+        const imgProps = pdf.getImageProperties(imgData);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        pdf.save('exported_file.pdf');
+      })
+      .catch(error => {
+        console.error('Error exporting to PDF:', error);
+      });
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <div id="contentToExport" dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      <button  onClick={exportToPdf}>Export as PDF</button>
     </div>
   );
 }
